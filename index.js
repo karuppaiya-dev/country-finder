4c9bac6eea21009189a9932916a6753ebc84f0d8
karuppaiya <karuppaiya.fullstackdev@gmail.com>
2026-05-05 18:16:59 +0530

    edit db

--- index.js
+++ index.js
@@ -7,14 +7,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const db = new pg.Client({
-  user:     process.env.DB_USER,
-  host:     process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port:     process.env.DB_PORT
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-
 db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
