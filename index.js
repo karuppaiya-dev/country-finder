707f1cccc228dea12599ca1589ecb91b4f709761
karuppaiya <karuppaiya.fullstackdev@gmail.com>
2026-05-05 18:38:36 +0530

    change the port variable.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 const db = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
